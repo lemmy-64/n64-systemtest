40ef1f150afb1ee3c867d558f8744eac222cd42a
Lemmy <lemmy64@posteo.net>
2022-03-30 17:49:51 -0700

    Make ISViewer access 32 bit
    ISViewer lives within ROM, so 8 bit writes are weird.

--- src/isviewer.rs
+++ src/isviewer.rs
@@ -2,16 +2,30 @@
 const ISVIEWER_WRITE_LEN: *mut u32 = 0xB3FF0014 as *mut u32;
 
 // Write text data into this buffer
-const ISVIEWER_BUFFER_START: *mut u8 = 0xB3FF0020 as *mut u8;
+const ISVIEWER_BUFFER_START: *mut u32 = 0xB3FF0020 as *mut u32;
 const ISVIEWER_BUFFER_LENGTH: usize = 0x200;
 
 // This method simply prints text without synchronization. This should only be used from within
 // the exception handler which can't wait for a lock
 pub fn text_out(s: &str) {
     for chunk in s.as_bytes().chunks(ISVIEWER_BUFFER_LENGTH) {
-        for i in 0..chunk.len() {
-            let b = chunk[i];
-            unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(b) };
+        // Write
+        let mut value = 0u32;
+        let mut shift = 24u32;
+        let mut i = 0;
+        for byte in chunk {
+            value |= (*byte as u32) << shift;
+            if shift == 0 {
+                unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(value) };
+                i += 1;
+                shift = 24;
+                value = 0;
+            } else {
+                shift -= 8;
+            }
+        }
+        if shift < 24 {
+            unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(value) };
         }
         unsafe { ISVIEWER_WRITE_LEN.write_volatile(chunk.len() as u32); }
     }
