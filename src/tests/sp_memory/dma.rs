fae5d4a43406d1695935b8d2ddbfe5e08ed7d65c
Lemmy <lemmy64@posteo.net>
2022-05-14 21:33:11 -0700

    Ensure that SP address reg is correct after DMA

--- src/tests/sp_memory/dma.rs
+++ src/tests/sp_memory/dma.rs
@@ -15,7 +15,7 @@ use crate::tests::soft_asserts::soft_assert_eq2;
 // - Length: To the written value 1 is added and then it is rounded up to the next multiple of 8 (e.g. 0..7 ==> 8 bytes, 8..15 => 16 bytes)
 // - A DMA goes either into IMEM or DMEM. If it overflows, it will overflow within that memory but never overlap into the other one
 
-fn dma_test<const N: usize>(source_index: usize, spmem_index: u32, length: u32, expected_start_offset: usize, expected: [[u16; 8]; N]) -> Result<(), String> {
+fn dma_test<const N: usize>(source_index: usize, spmem_index: u32, length: u32, expected_start_offset: usize, expected_sp_address_after_dma: u32, expected: [[u16; 8]; N]) -> Result<(), String> {
     // Create some test data. Use uncached memory to ensure the DMA engine can see it
     // without us having to flush any caches first
     let mut source_data: [[u16; 8]; 4] = [[0u16; 8]; 4];
@@ -42,6 +42,8 @@ fn dma_test<const N: usize>(source_index: usize, spmem_index: u32, length: u32, 
         soft_assert_eq2(SPMEM::read_vector16_from_dmem_or_imem(expected_start_offset + i * 0x10), expected[i], || format!("SPMEM[0x{:x}] after DMA", expected_start_offset + i * 0x10))?;
     }
 
+    soft_assert_eq2(RSP::sp_address(), expected_sp_address_after_dma, || "SP-Address after DMA".to_string())?;
+
     Ok(())
 }
 
@@ -55,7 +57,7 @@ impl Test for SPDMA0_8_7 {
     fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
-        dma_test(0, 8, 7, 0, [
+        dma_test(0, 8, 7, 0, 0x10,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0x0123, 0x4567, 0x89AB, 0xCDEF],
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
         Ok(())
@@ -72,7 +74,7 @@ impl Test for SPDMA0_12_7D {
     fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
-        dma_test(0, 12, 7, 0, [
+        dma_test(0, 12, 7, 0, 0x10,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0x0123, 0x4567, 0x89AB, 0xCDEF],
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
         Ok(())
@@ -89,7 +91,7 @@ impl Test for SPDMA0_12_7I {
     fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
-        dma_test(0, 0x100B, 7, 0x1000, [
+        dma_test(0, 0x100B, 7, 0x1000, 0x1010,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0x0123, 0x4567, 0x89AB, 0xCDEF],
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
         Ok(())
@@ -106,7 +108,7 @@ impl Test for SPDMA4_8_7 {
     fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
-        dma_test(4, 8, 7, 0, [
+        dma_test(4, 8, 7, 0, 0x10,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0x0123, 0x4567, 0x89AB, 0xCDEF],
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
         Ok(())
@@ -123,7 +125,7 @@ impl Test for SPDMA0_8_11D {
     fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
-        dma_test(0, 8, 11, 0, [
+        dma_test(0, 8, 11, 0, 0x18,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0x0123, 0x4567, 0x89AB, 0xCDEF],
             [0xFEDC, 0x89BA, 0x7654, 0x3210, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
         Ok(())
@@ -140,9 +142,31 @@ impl Test for SPDMA0_8_11I {
     fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
-        dma_test(0, 0x1008, 11, 0x1000, [
+        dma_test(0, 0x1008, 11, 0x1000, 0x1018,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0x0123, 0x4567, 0x89AB, 0xCDEF],
             [0xFEDC, 0x89BA, 0x7654, 0x3210, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
+        Ok(())
+    }
+}
+
+pub struct SPDMAIntoDMEMUntilEnd {}
+
+impl Test for SPDMAIntoDMEMUntilEnd {
+    fn name(&self) -> &str { "spmem: DMA RDRAM -> DMEM (until the end)" }
+
+    fn level(&self) -> Level { Level::Weird }
+
+    fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
+
+    fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
+        // expected will read from both DMEM and IMEM. Ensure that nothing spilled into IMEM
+        dma_test(0, 0xFF0, 15, 0xFE0, 0x0,[
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
+            [0x0123, 0x4567, 0x89AB, 0xCDEF, 0xFEDC, 0x89BA, 0x7654, 0x3210],
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
+
         Ok(())
     }
 }
@@ -158,7 +182,7 @@ impl Test for SPDMAIntoDMEMWithOverflow {
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
         // expected will read from both DMEM and IMEM. Ensure that nothing spilled into IMEM
-        dma_test(0, 0xFF0, 31, 0xFE0, [
+        dma_test(0, 0xFF0, 31, 0xFE0, 0x10,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
             [0x0123, 0x4567, 0x89AB, 0xCDEF, 0xFEDC, 0x89BA, 0x7654, 0x3210],
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
@@ -167,6 +191,28 @@ impl Test for SPDMAIntoDMEMWithOverflow {
 
         // But also ensure that DMEM properly overflowed
         soft_assert_eq2(SPMEM::read_vector16_from_dmem(0), [0x1212, 0x3434, 0x4545, 0x5656, 0x6767, 0x7878, 0x8989, 0x9A9A], || "SPMEM[0x0] after DMA".to_string())?;
+        Ok(())
+    }
+}
+
+pub struct SPDMAIntoIMEMUntilEnd {}
+
+impl Test for SPDMAIntoIMEMUntilEnd {
+    fn name(&self) -> &str { "spmem: DMA RDRAM -> IMEM (until the end)" }
+
+    fn level(&self) -> Level { Level::Weird }
+
+    fn values(&self) -> Vec<Box<dyn Any>> { Vec::new() }
+
+    fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
+        // expected will read from both DMEM and IMEM. Ensure that nothing spilled into IMEM
+        dma_test(0, 0x1FF0, 15, 0x1FE0, 0x1000,[
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
+            [0x0123, 0x4567, 0x89AB, 0xCDEF, 0xFEDC, 0x89BA, 0x7654, 0x3210],
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
+            [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF]])?;
+
         Ok(())
     }
 }
@@ -182,7 +228,7 @@ impl Test for SPDMAIntoIMEMWithOverflow {
 
     fn run(&self, _value: &Box<dyn Any>) -> Result<(), String> {
         // expected will read from both IMEM and DMEM. Ensure that nothing spilled into DMEM
-        dma_test(0, 0x1FF0, 31, 0x1FE0, [
+        dma_test(0, 0x1FF0, 31, 0x1FE0, 0x1010,[
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
             [0x0123, 0x4567, 0x89AB, 0xCDEF, 0xFEDC, 0x89BA, 0x7654, 0x3210],
             [0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF, 0xBADD, 0xECAF],
