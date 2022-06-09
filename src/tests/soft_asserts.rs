390f56522b9591adbfc6e70c0d58f378f3842e77
Lemmy <lemmy64@posteo.net>
2022-06-09 11:35:24 -0700

    Reintroduce WiredRandom and WiredOutOfBoundsRandom tests
    These were removed as we had cycle-accurate test. However, not every emulator
    is cycle accurate (and for now cycle-accurate tests are disabled), so the old style of testing has some advantages as well.

--- src/tests/soft_asserts.rs
+++ src/tests/soft_asserts.rs
@@ -44,6 +44,15 @@ pub fn soft_assert_neq<T: Display + LowerHex + PartialEq + Eq>(v1: T, v2: T, hel
     }
 }
 
+/// Tests if `v1 >= v2`.
+pub fn soft_assert_greater_or_equal(v1: u32, v2: u32, help: &str) -> Result<(), String> {
+    if v1 >= v2 {
+        Ok(())
+    } else {
+        Err(format!("a >= b expected, but a={} b={} (hex: a=0x{:x} b=0x{:x}). {}", v1, v2, v1, v2, help))
+    }
+}
+
 /// Tests if `v1 < v2`.
 pub fn soft_assert_less(v1: u32, v2: u32, help: &str) -> Result<(), String> {
     if v1 < v2 {
