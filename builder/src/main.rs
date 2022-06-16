30a6999040ca1e2e0785f3172ca5b52acdb741b1
bigbass1997 <bigbass1997.website@gmail.com>
2022-06-15 22:25:17 -0500

    Forgot about the debug IPL3. Will be configurable soon.

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 fn main() {
     let elf = nust64::elf::Elf::build(&PathBuf::from("n64-systemtest"), Some(&["--features", "default_tests"])).unwrap();
-    let rom = nust64::rom::Rom::new(&elf, include_bytes!("../../lemon-ipl3/lemon-ipl3.bin").to_owned(), None);
+    let rom = nust64::rom::Rom::new(&elf, include_bytes!("../../mini-ipl3/mini-ipl3.bin").to_owned(), None);
     
     std::fs::write("n64-systemtest.n64", rom.to_vec()).unwrap();
 }
