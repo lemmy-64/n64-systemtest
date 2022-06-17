c0b2b9eca536bbc5e742839f8f9c4bb78716a48a
bigbass1997 <bigbass1997.website@gmail.com>
2022-06-16 22:11:11 -0500

    Fix default IPL3, and warn if IPL3 file cannot be found.

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -16,15 +16,15 @@ fn main() {
         .next_line_help(true)
         .get_matches();
     
-    let ipl3_path = matches.value_of("ipl3").unwrap_or("../../mini-ipl3/mini-ipl3.bin");
-    let ipl3 = std::fs::read(ipl3_path).unwrap_or_default();
+    let ipl3_path = matches.value_of("ipl3").unwrap_or("mini-ipl3/mini-ipl3.bin");
+    let ipl3 = std::fs::read(ipl3_path).expect(&format!("IPL3 file doesn't exist: {}", ipl3_path));
     
     let mut features = matches.values_of_lossy("features").unwrap_or(vec!["default_tests".to_owned()]);
     features.insert(0, "--features".to_owned());
     let features = &features.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
     
     let elf = nust64::elf::Elf::build(&PathBuf::from("n64-systemtest"), Some(&features)).unwrap();
-    let rom = nust64::rom::Rom::new(&elf, ipl3.try_into().unwrap_or([0u8; 4032]), None);
+    let rom = nust64::rom::Rom::new(&elf, ipl3.try_into().expect("Failed to cast into array. Is input not exactly 4032 bytes?"), None);
     
     std::fs::write("n64-systemtest.n64", rom.to_vec()).unwrap();
 }
